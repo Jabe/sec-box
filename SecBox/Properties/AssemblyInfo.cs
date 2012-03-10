32de11bd8cb82f6e133f859baaf58d6bcb869089
jp.berdel <jp.berdel@googlemail.com>
2012-03-10 12:32:41 +0100

    added semantic versioning and proper assembly info

--- SecBox/Properties/AssemblyInfo.cs
+++ SecBox/Properties/AssemblyInfo.cs
@@ -1,36 +1,33 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SecurePasswordBox")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle("SecBox")]
+[assembly: AssemblyDescription("A password box implementation which behaves like a normal textbox, " +
+                               "but stores all data in a SecureString.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SecurePasswordBox")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("https://github.com/Jabe/sec-box/")]
+[assembly: AssemblyProduct("SecBox")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("0088e057-5bc3-4def-884a-0c55021ee626")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// semantic versioning: http://semver.org/
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0-beta.1")]
+[assembly: AssemblyInformationalVersion("1.0 beta")]
